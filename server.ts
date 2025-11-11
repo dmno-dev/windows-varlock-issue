76e7ed7ba0c80d7ce382bc451dcbe7dd3e12a54e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2025-11-10 20:41:41 -0600

    Use supertest

--- server.ts
+++ server.ts
@@ -1,26 +1,30 @@
 import http from 'node:http';
+import { fileURLToPath } from 'node:url';
 
-const server = http.createServer((request, response) => {
+export const server = http.createServer((request, response) => {
   if (request.method === 'HEAD') {
     response.writeHead(200);
     response.end();
   } else if (request.method === 'GET') {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end(`Hello, world!\n`);
+    response.end(`Hello, world!`);
   } else {
     response.writeHead(405, { Allow: 'GET, HEAD' });
     response.end('Method Not Allowed');
   }
 });
 
-server.listen(
-  {
-    host: process.env.HOST ?? 'localhost',
-    port: Number.parseInt(process.env.PORT!, 10) || 3000,
-  },
-  () => {
-    console.log(
-      `Server running at http://${process.env.HOST}:${process.env.PORT}/`,
-    );
-  },
-);
+if (fileURLToPath(import.meta.url) === process.argv.at(1)) {
+  const host = process.env.HOST ?? 'localhost';
+  const port = Number.parseInt(process.env.PORT!, 10) || 3000;
+
+  server.listen(
+    {
+      host,
+      port,
+    },
+    () => {
+      console.log(`Server running at http://${host}:${port}/`);
+    },
+  );
+}
--- test.spec.ts
+++ test.spec.ts
@@ -1,9 +1,13 @@
-import { test, type TestContext } from 'node:test';
+import { test } from 'node:test';
 
-test('http server test', async (context: TestContext) => {
-  const host = process.env.HOST ?? 'localhost';
-  const port = Number.parseInt(process.env.PORT!, 10) || 3000;
-  const response = await fetch(`http://${host}:${port}`);
+import request from 'supertest';
 
-  context.assert.equal(response.status, 200);
+import { server } from './server.ts';
+
+test('http server test', async () => {
+  await request(server)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', 'text/plain')
+    .expect('Hello, world!');
 });
